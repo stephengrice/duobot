9fdb3d2c83cb7c1ec0e9e55007191d8e63516cdb
Stephen Grice <stephengrice@live.com>
2020-03-20 20:15:30 -0400

    Improve run script

--- run.bash
+++ run.bash
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-./src/duobot.py
+if [ $(command -v python) ]; then
+  python src/duobot.py
+elif [ $(command -v python3) ]; then
+  python3 src/duobot.py
+else
+  echo Error: Python not installed.
+  echo Please install python or python3 to use this program.
+fi
+
