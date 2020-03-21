8a7ba4a50f71b08e4f953e1a8fa8a97525df66c8
Stephen Grice <stephengrice@live.com>
2020-03-20 21:28:20 -0400

    Fix run script

--- run.bash
+++ run.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+ENTRY_POINT=src/main/duobot.py
+
 if [ $(command -v python) ]; then
-  python src/duobot.py
+  python $ENTRY_POINT
 elif [ $(command -v python3) ]; then
-  python3 src/duobot.py
+  python3 $ENTRY_POINT
 else
   echo Error: Python not installed.
   echo Please install python or python3 to use this program.
