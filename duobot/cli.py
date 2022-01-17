e1f47c0138ce7be2758aa126c040e733d447dc83
Stephen Grice <stephengrice@live.com>
2022-01-17 12:32:58 -0500

    Get through the first question

--- duobot/cli.py
+++ duobot/cli.py
@@ -7,9 +7,26 @@ REPO_ROOT = os.path.dirname(os.path.dirname(__file__))
 CFG_FILE = os.path.join(REPO_ROOT, 'config', 'config.yml')
 LOGIN_URL = "https://www.duolingo.com/"
 
+BRAIN_CHINESE = os.path.join(REPO_ROOT, 'brain', 'Chinese.csv')
+BRAIN_SEP = "|"
+
+def get_brain():
+    result = []
+    with open(BRAIN_CHINESE, 'r') as f:
+        for line in f:
+            result.append(line.split(BRAIN_SEP))
+    return result
+
+def search_brain(brain, query):
+    for row in brain:
+        if row[0] == query:
+            return row[1]
+        elif row[1] == query:
+            return row[0]
+    return None
 
 def main():
-    config = yaml.load(CFG_FILE, yaml.SafeLoader)
+    brain = get_brain()
     options = webdriver.firefox.options.Options()
     # if not DEBUG or ci: options.headless = True
     driver = webdriver.Firefox(log_path='geckodriver.log', options=options)
@@ -21,11 +38,17 @@ def main():
     driver.find_element_by_css_selector("[data-test='register-button']").click()
     driver.find_element_by_css_selector("[data-test='skill-icon']").click()
     driver.find_element_by_css_selector("[data-test='start-button']").click()
-    print("searching for prompt...")
-    prompt = driver.find_element_by_css_selector("[dir=ltr]").text
+    prompt = driver.find_element_by_css_selector("div[dir='ltr']").text
+    prompt = prompt.lower().replace('!','').replace('.','').replace('?','')
     print(prompt)
-    print("printed prompt")
-    breakpoint()
+    answer = search_brain(brain, prompt)
+    print("Found answer: %s" % answer)
+    for btn in driver.find_elements_by_css_selector("[data-test='challenge-tap-token']"):
+        print("Checking btn %s" % btn.text)
+        if btn.text == answer:
+            print("clicking")
+            btn.click()
+    driver.find_element_by_css_selector("[data-test='player-next']").click()
+    driver.find_element_by_css_selector("[data-test='player-next']").click()
     while True:
-        print("sleeping")
         time.sleep(1)
