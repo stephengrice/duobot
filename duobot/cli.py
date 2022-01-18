f43e569d46ae7f399803e96ace208ce29c434752
Stephen Grice <stephengrice@live.com>
2022-01-18 15:36:37 -0500

    Add handling for more question types

--- duobot/cli.py
+++ duobot/cli.py
@@ -2,6 +2,7 @@ import os
 import time
 import yaml
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 REPO_ROOT = os.path.dirname(os.path.dirname(__file__))
 CFG_FILE = os.path.join(REPO_ROOT, 'config', 'config.yml')
@@ -18,12 +19,13 @@ def get_brain():
     return result
 
 def search_brain(brain, query):
+    answers = []
     for row in brain:
         if row[0] == query:
-            return row[1]
+            answers.append(row[1])
         elif row[1] == query:
-            return row[0]
-    return None
+            answers.append(row[0])
+    return answers
 
 def main():
     brain = get_brain()
@@ -38,17 +40,41 @@ def main():
     driver.find_element_by_css_selector("[data-test='register-button']").click()
     driver.find_element_by_css_selector("[data-test='skill-icon']").click()
     driver.find_element_by_css_selector("[data-test='start-button']").click()
-    prompt = driver.find_element_by_css_selector("div[dir='ltr']").text
-    prompt = prompt.lower().replace('!','').replace('.','').replace('?','')
-    print(prompt)
-    answer = search_brain(brain, prompt)
-    print("Found answer: %s" % answer)
-    for btn in driver.find_elements_by_css_selector("[data-test='challenge-tap-token']"):
-        print("Checking btn %s" % btn.text)
-        if btn.text == answer:
-            print("clicking")
-            btn.click()
-    driver.find_element_by_css_selector("[data-test='player-next']").click()
-    driver.find_element_by_css_selector("[data-test='player-next']").click()
+
     while True:
-        time.sleep(1)
+        try:
+            header = driver.find_element_by_css_selector("[data-test='challenge-header']").text
+            print("Header: %s" % header)
+            if 'Select the correct character' in header:
+                prompt = header.split("“")[1].split("”")[0]
+            else:
+                prompt = driver.find_element_by_css_selector("div[dir='ltr'],span[dir='ltr']").text
+            prompt = prompt.lower()
+            for filter_char in ['!', '.', '?', u'\uFF01']:
+                prompt = prompt.lower().replace(filter_char, '')
+            print("Prompt: %s" % prompt)
+            answers = search_brain(brain, prompt)
+            print("Found answers: %s" % answers)
+            for btn in driver.find_elements_by_css_selector("[data-test='challenge-tap-token']"):
+                print("Checking btn %s" % btn.text)
+                if btn.text in answers:
+                    print("clicking")
+                    btn.click()
+            for btn in driver.find_elements_by_css_selector("[data-test='challenge-judge-text']"):
+                print("Checking 'challenge-judge-text' %s" % btn.text)
+                if btn.text in answers:
+                    print("clicking")
+                    btn.click()
+            for btn in driver.find_elements_by_css_selector("[data-test='challenge-choice-card'] div:first-child"):
+                print("Checking 'challenge-choice-card' %s" % btn.text)
+                if btn.text in answers:
+                    print("clicking")
+                    btn.click()
+            textarea = driver.find_element_by_css_selector("[data-test='challenge-translate-input'")
+            textarea.send_keys(answers[0]) # TODO detect which it's asking for - english or chinese
+        except NoSuchElementException:
+            print("No such element")
+        driver.find_element_by_css_selector("[data-test='player-next']").click()
+        driver.find_element_by_css_selector("[data-test='player-next']").click()
+        print("----- sleeping -----")
+        time.sleep(3)
